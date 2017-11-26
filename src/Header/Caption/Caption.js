4e34821b8a354f2f81fbb0dc0916328eb208f3c2
aleksandrlepin <yanektulho@gmail.com>
2017-11-26 22:29:59 +0200

    extract Menu & Caption component from Header

---
+++ src/Header/Caption/Caption.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Caption.css';
+
+function Caption () {
+    return (
+        <div className="caption">
+            <h1>Name Surname</h1>
+            <p>Front-end developer</p>
+        </div>
+    );
+}
+
+export default Caption;
